27250144f747d4c4594fdd7db42f78ca3441eb2b
yungene <32654121+yungene@users.noreply.github.com>
2019-11-08 11:48:00 +0000

    Fix typo in SegmentTree.

--- code/SegmentTree.java
+++ code/SegmentTree.java
@@ -25,8 +25,6 @@ public class SegmentTree {
         tree[i] = tree[2*i] + tree[2*i+1]; 
       }
     }
-    
-    
     // get the sum from arr[l] to arr[r]
     int sum(int l, int r) {
       return sum(1,0,n-1,l,r);
@@ -38,25 +36,15 @@ public class SegmentTree {
       }
       if(tl == l && tr == r) {
         return tree[i];
-      } else {
-        int tm = (tl+tr)/2;
-        if(r<=tm) {
-          return sum(2*i,tl,tm,l,r);
-        } else if (l > tm) {
-          return sum(2*i+1,tm+1,tr,l,r);
-        } else {
-            int ls = sum(2*i,tl,tm,l,tm);
-            int rs = sum(2*i+1,tm+1,tr,tm+1,r);
-            return ls + rs;
-        } 
-      }
+      } 
+      int tm = (tl + tr) / 2;
+        return sum(i*2, tl, tm, l, Math.min(r, tm))
+               + sum(i*2+1, tm+1, tr, Math.max(l, tm+1), r);
     } 
     
     void update(int pos, int newVal) {
       update(1,0,n-1,pos,newVal);
-      
     }   
-    // up
     private void update(int i, int tl, int tr, int pos, int newVal) {
       if(tl==tr) {
         tree[i]=newVal;
@@ -65,12 +53,10 @@ public class SegmentTree {
         if(pos<=tm) {
           update(i*2,tl,tm,pos,newVal);
         } else {
-          update(i*2+1,tl,tm,pos,newVal);
+          update(i*2+1,tm+1,tr,pos,newVal);
         }
         tree[i] = tree[2*i] + tree[2*i+1];
       }
     }
-    
   }
-
 }
